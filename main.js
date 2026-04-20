3c46c9028be299698e633142f4fe162d05d90167
federicosimone <federico.simone15@gmail.com>
2026-04-20 21:48:47 +0200

    errors

--- main.js
+++ main.js
@@ -4,22 +4,29 @@ const API_URL = "http://localhost:3333";
 
 
 async function getDashboardData(query) {
-    const destinazionePromise = fetch(`${API_URL}/destinations?search=${query}`).then(destinazione => destinazione.json()); //promise per destinazione
-    const meteoPromise = fetch(`${API_URL}/weathers?search=${query}`).then(meteo => meteo.json()); //promise per meteo 
-    const airportPromise = fetch(`${API_URL}/airports?search=${query}`).then(airport => airport.json()); //promise per aeroporto 
+    try {
+        const destinazionePromise = fetch(`${API_URL}/destinations?search=${query}`).then(destinazione => destinazione.json()); //promise per destinazione
+        const meteoPromise = fetch(`${API_URL}/weathers?search=${query}`).then(meteo => meteo.json()); //promise per meteo 
+        const airportPromise = fetch(`${API_URL}/airports?search=${query}`).then(airport => airport.json()); //promise per aeroporto 
 
-    const [[cityData], [weatherData], [airportData]] = await Promise.all([destinazionePromise, meteoPromise, airportPromise])
+        const [[cityData], [weatherData], [airportData]] = await Promise.all([destinazionePromise, meteoPromise, airportPromise])
 
-    let info = {};
 
-    info.city = cityData.name
-    info.country = cityData.country
-    info.temperature = weatherData.temperature
-    info.weather = weatherData.weather_description
-    info.airport = airportData.name
+        let info = {};
 
-    return info;
+        info.city = cityData.name
+        info.country = cityData.country
+        info.temperature = weatherData.temperature
+        info.weather = weatherData.weather_description
+        info.airport = airportData.name
+
+        return info;
+    } catch (err) {
+        throw new Error(`Errore recuperando i dati : ${err.message}`)
+    }
+
 }
+
 
 getDashboardData('london')
     .then(data => console.log(`${data.city} is a city of ${data.country}. Today the temperature is ${data.temperature} degrees and the weather is ${data.weather}. The most important airport is ${data.airport}`))
